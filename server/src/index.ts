7e5f69e55a797dfe6f952164b194c2c1ffe3a2b5
aruyamam <aruyamam@gmail.com>
2019-05-07 10:57:12 +0900

    The Public Directory

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,12 +5,24 @@ import Home from './client/components/Home';
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get(
    '/',
    (req, res): void => {
       const content = renderToString(React.createElement(Home));
 
-      res.send(content);
+      const html = `
+         <html>
+            <head></head>
+            <body>
+               <div>${content}</div>
+               <script src="bundle.js"></script>
+            </body>
+         </html>
+      `;
+
+      res.send(html);
    },
 );
 
