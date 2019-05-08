26baf6fd001968fb3b4b747f8afd33dbf09fc895
aruyamam <aruyamam@gmail.com>
2019-05-08 10:55:37 +0900

    Renderer Helper

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,5 @@
 import express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import Home from './client/components/Home';
+import renderer from './helpers/renderer';
 
 const app = express();
 
@@ -10,19 +8,7 @@ app.use(express.static('public'));
 app.get(
    '/',
    (req, res): void => {
-      const content = renderToString(React.createElement(Home));
-
-      const html = `
-         <html>
-            <head></head>
-            <body>
-               <div id="root">${content}</div>
-               <script src="bundle.js"></script>
-            </body>
-         </html>
-      `;
-
-      res.send(html);
+      res.send(renderer);
    },
 );
 
