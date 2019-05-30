5ae0b018f2367e0931c89e1ea7972cd8defb23ce
aruyamam <aruyamam@gmail.com>
2019-05-30 09:52:39 +0900

    Header Styling

--- server/src/client/pages/HomePage.tsx
+++ server/src/client/pages/HomePage.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
 const Home = (): JSX.Element => (
-   <div>
-      <div>I&apos;m the VERY VERY BEST home component</div>
-      <button type="button" onClick={(): void => console.log('Hi there!')}>
-         Press me!
-      </button>
+   <div className="center-align" style={{ marginTop: '200px' }}>
+      <h3>Welcome</h3>
+      <p>Check out these awesome features</p>
    </div>
 );
 
